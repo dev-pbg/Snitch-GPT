5f6ccde7abc98459757f750bbe6f3dd667848d9f
Prithvi - Mac <kpatpbg@gmail.com>
2023-11-11 09:30:33 +0530

    Initial Commit

---
+++ Snitch GPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snitch GPT
+//
+//  Created by Prithvi Bharadwaj on 11/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Snitch GPTUITests/Snitch_GPTUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Snitch_GPTUITestsLaunchTests.swift
+//  Snitch GPTUITests
+//
+//  Created by Prithvi Bharadwaj on 11/11/23.
+//
+
+import XCTest
+
+final class Snitch_GPTUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
